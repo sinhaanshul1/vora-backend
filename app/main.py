2ecbf00793e670a9b02c362ff00dcb2235b2c8f4
Anshul Sinha <sinha.anshul1@gmail.com>
2025-11-22 12:33:10 -0800

    fixed env variable loading

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import os
 from supabase import create_client, Client
 from dotenv import load_dotenv
 
-load_dotenv(override=True)
+# load_dotenv(override=True)
 
 url: str = os.getenv("SUPABASE_URL")
 key: str = os.getenv("SUPABASE_KEY")
